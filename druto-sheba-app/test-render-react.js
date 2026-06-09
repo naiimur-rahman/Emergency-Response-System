ec12b4397517458ffc0802e2a07f4b7830b6110a
Naimur Rahman <rahmansohan454@gmail.com>
2026-06-10 05:22:07 +0600

    ok

--- druto-sheba-app/test-render-react.js
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
-
-try {
-  const div = React.createElement('div', { style: { width: 'NaN%' } });
-  console.log(ReactDOMServer.renderToString(div));
-} catch (e) {
-  console.error("ERROR:", e.message);
-}
